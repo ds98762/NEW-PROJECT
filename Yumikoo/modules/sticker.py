d9950eceaa129fc2b52f3adb2e2a7230de87ee55
Dhiman <103347834+ds98762@users.noreply.github.com>
2023-12-10 12:29:39 +0530

    Update sticker.py

--- Yumikoo/modules/sticker.py
+++ Yumikoo/modules/sticker.py
@@ -56,7 +56,7 @@ async def _packkang(Yumikoo :Yumikoo,message):
     if message.reply_to_message.sticker.is_animated or  message.reply_to_message.sticker.is_video:
         return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
     if len(message.command) < 2:
-        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@YumikooRobot'
+        pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@Rashmika_Robot'
     else :
         pack_name = message.text.split(maxsplit=1)[1]
     short_name = message.reply_to_message.sticker.set_name
